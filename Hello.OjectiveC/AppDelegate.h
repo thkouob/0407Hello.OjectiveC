43c2a5173f553aa8db2baca5e4a0f7df45d22027
thkouobchen <thkouobchen@thkouobchende-macbook-pro.local>
2016-04-07 20:35:14 +0800

    Initial Commit

---
+++ Hello.OjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello.OjectiveC
+//
+//  Created by thkouobchen on 2016/4/7.
+//  Copyright © 2016年 thkouobchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello.OjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hello.OjectiveC
+//
+//  Created by thkouobchen on 2016/4/7.
+//  Copyright © 2016年 thkouobchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
